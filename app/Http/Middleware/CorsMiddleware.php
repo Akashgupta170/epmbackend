adb1987f39ea729308ab75896d64658c22f8a1a1
Akashgupta170 <akash@techarchsoftwares.in>
2025-04-30 13:37:14 +0530

    middlware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,13 +15,6 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->getMethod() === 'OPTIONS') {
-            return response('', 204)
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        }
-
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
